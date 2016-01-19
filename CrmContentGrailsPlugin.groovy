acfedeba3627f2b5e9c9dac87b66624a7b343f2e
Goran Ehrsson <goran@technipelago.se>
2016-01-19 13:51:26 +0100

    Version 2.4.3 published to grails.org

--- CrmContentGrailsPlugin.groovy
+++ CrmContentGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile
 
 class CrmContentGrailsPlugin {
     def groupId = ""
-    def version = "2.4.3-SNAPSHOT"
+    def version = "2.4.3"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def observe = ['controllers']
