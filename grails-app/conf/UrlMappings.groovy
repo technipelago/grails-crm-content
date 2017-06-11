e968368fb4388f4478acd29e3a91598bc693914c
Goran Ehrsson <goran@technipelago.se>
2017-06-11 22:54:37 +0200

    Cleanup

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,6 @@
 class UrlMappings {
 
     static mappings = {
-        name 'test': "/hej/hopp/$id?" {
-            controller = 'hej'
-            action = 'hopp'
-            constraints {
-                id(matches: /\d+/)
-            }
-        }
         "/$controller/$action?/$id?" {
             constraints {
                 // apply constraints here
