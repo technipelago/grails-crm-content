d611719c6ca962c9c2df7851668c980dc6ff2782
Goran Ehrsson <goran@technipelago.se>
2017-09-05 13:39:37 +0200

    Ooops 'test' mapping required by integration test

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,13 @@
 class UrlMappings {
 
     static mappings = {
+        name 'test': "/hej/hopp/$id?" {
+            controller = 'hej'
+            action = 'hopp'
+            constraints {
+                id(matches: /\d+/)
+            }
+        }
         "/$controller/$action?/$id?" {
             constraints {
                 // apply constraints here
